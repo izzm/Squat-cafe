4d2261258a5cd962d6efb7d875654c5deca6236b
Izoria Vladislav <vladislav.izoria@gmail.com>
2012-01-18 22:32:06 +0400

    Update cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,50 +1,48 @@
-# _your_login_ - Поменять на ваш логин в панели управления
-# _your_project_ - Поменять на имя вашего проекта
-# _your_server_ - Поменять на имя вашего сервера (Указано на вкладке "FTP и SSH" вашей панели управления)
-# set :repository - Установить расположение вашего репозитория
 # У вас должна быть настроена авторизация ssh по сертификатам
 
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+set :application, "squat"
 
-require "bundler/capistrano"
-require "rvm/capistrano"
-set :rvm_ruby_string, '1.9.2'
-
-your_login = "squat"
-your_project = "squat"
-your_server = "lithium"
-
-set :application, "RailsShop"
+# настройка системы контроля версий и репозитария, по умолчанию - git, если используется иная система версий, нужно изменить значение scm
 set :scm, :git
-set :repository,  "git://github.com/izzm/Squat-cafe.git"
+set :repository,  "тут нужно указать путь к вашему репозитарию"
 
-dpath = "/home/hosting_#{your_login}/projects/#{your_project}"
-
-set :user, "hosting_#{your_login}"
+set :user, "hosting_squat"
 set :use_sudo, false
-set :deploy_to, dpath
+set :deploy_to, "/home/hosting_squat/projects/squat"
 
-role :web, "#{your_server}.locum.ru" # Your HTTP server, Apache/etc
-role :app, "#{your_server}.locum.ru"  # This may be the same as your `Web` server
-role :db,  "#{your_server}.locum.ru", :primary => true # This is where Rails migrations will run
 
-after "deploy:update_code", :copy_database_config
-after "deploy", "deploy:migrations"
+role :web, "lithium.locum.ru"   # Your HTTP server, Apache/etc
+role :app, "lithium.locum.ru"   # This may be the same as your `Web` server
+role :db,  "lithium.locum.ru", :primary => true # This is where Rails migrations will run
 
-task :copy_database_config, roles => :app do
-  db_config = "#{shared_path}/config/database.yml"
-  run "cp #{db_config} #{release_path}/config/database.yml"
-end
+# эта секция для того, чтобы вы не хранили доступ к базе в системе контроля версий. Поместите dayabase.yml в shared,
+# чтобы он копировался в нужный путь при каждом выкладывании новой версии кода
+# так лучше с точки зрения безопасности, но если не хотите - прсото закомментируйте этот таск
 
-set :unicorn_conf, "/etc/unicorn/#{your_project}.#{your_login}.rb"
-set :unicorn_pid, "/var/run/unicorn/#{your_project}.#{your_login}.pid"
-set :unicorn_rails, "(cd #{deploy_to}/current; rvm use 1.9.2 do bundle exec unicorn_rails -Dc #{unicorn_conf})"
+
+# Если хотите поместить конфиг в shared и не хранить его в системе контроя версий - раскомментируйте следующие строки
+
+#after "deploy:update_code", :copy_database_config
+
+#task :copy_database_config, roles => :app do
+#  db_config = "#{shared_path}/database.yml"
+#  run "cp #{db_config} #{release_path}/config/database.yml"
+#end
+
+set :unicorn_conf, "/etc/unicorn/squat.squat.rb"
+set :unicorn_pid, "/var/run/unicorn/squat.squat.pid"
+
+
+
+  set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use 1.9.2 do bundle exec unicorn_rails -Dc #{unicorn_conf})"
+
+
 
 # - for unicorn - #
 namespace :deploy do
   desc "Start application"
   task :start, :roles => :app do
-    run unicorn_rails
+    run unicorn_start_cmd
   end
 
   desc "Stop application"
@@ -54,6 +52,6 @@ namespace :deploy do
 
   desc "Restart Application"
   task :restart, :roles => :app do
-    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_rails}"
+    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_start_cmd}"
   end
 end
