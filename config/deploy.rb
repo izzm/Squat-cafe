acb695cb085e99c7709cd30343a9b64570d26c02
Izoria Vladislav <vladislav.izoria@gmail.com>
2012-01-18 23:16:32 +0400

    Update cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # У вас должна быть настроена авторизация ssh по сертификатам
 
-set :application, "squat"
+set :application, "squat-cafe"
 
 # настройка системы контроля версий и репозитария, по умолчанию - git, если используется иная система версий, нужно изменить значение scm
 set :scm, :git
@@ -8,7 +8,7 @@ set :repository,  "git://github.com/izzm/Squat-cafe.git"
 
 set :user, "hosting_squat"
 set :use_sudo, false
-set :deploy_to, "/home/hosting_squat/projects/squat"
+set :deploy_to, "/home/hosting_squat/projects/squat-cafe"
 
 
 role :web, "lithium.locum.ru"   # Your HTTP server, Apache/etc
@@ -22,15 +22,15 @@ role :db,  "lithium.locum.ru", :primary => true # This is where Rails migrations
 
 # Если хотите поместить конфиг в shared и не хранить его в системе контроя версий - раскомментируйте следующие строки
 
-after "deploy:update_code", :copy_database_config
+#after "deploy:update_code", :copy_database_config
 
-task :copy_database_config, roles => :app do
-  db_config = "#{shared_path}/config/database.yml"
-  run "cp #{db_config} #{release_path}/config/database.yml"
-end
+#task :copy_database_config, roles => :app do
+#  db_config = "#{shared_path}/database.yml"
+#  run "cp #{db_config} #{release_path}/config/database.yml"
+#end
 
-set :unicorn_conf, "/etc/unicorn/squat.squat.rb"
-set :unicorn_pid, "/var/run/unicorn/squat.squat.pid"
+set :unicorn_conf, "/etc/unicorn/squat-cafe.squat.rb"
+set :unicorn_pid, "/var/run/unicorn/squat-cafe.squat.pid"
 
 
 
