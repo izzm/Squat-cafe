c39ab35fd91b3473e8805c2f3fb0e301cd5f9061
Izoria Vladislav <vladislav.izoria@gmail.com>
2012-01-25 20:33:02 +0400

    Float menu

--- app/assets/javascripts/site/scripts.js
+++ app/assets/javascripts/site/scripts.js
@@ -1,3 +1,27 @@
 $(function() {
+  var $fixed = $('#menu_fixed_scroll');
+  var $slave_fixed = $('#sidebar_fixed_scroll');
+  var menu_offset = $fixed.offset().top;
+  var menu_height = $fixed.outerHeight();
+  var sidebar_offset = $slave_fixed.offset().left;
 
+  $(window).scroll(function() {
+    var page_scroll = $(this).scrollTop();
+    var offset = page_scroll - menu_offset;
+
+    if(page_scroll > menu_offset) {
+      $fixed.css({
+        position: 'fixed'
+      });
+      $slave_fixed.css({
+        position: 'fixed',
+        top: menu_height,
+        left: sidebar_offset - 20
+      });
+    } else {
+      $fixed.css({top: '0px', position: 'relative'});
+      $slave_fixed.css({top: '0px', left: '0px', position: 'relative'});
+    }
+
+  });
 });
