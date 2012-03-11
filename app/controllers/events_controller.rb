b46caf7837682234601f1707da23b146f6a07c63
Vladislav Izoria <vizoria@MacBook-Air-Vladislav.local>
2012-03-11 22:59:51 +0400

    Motherfuching facebook buttons

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,11 @@
 class EventsController < ApplicationController
   def index
     @events = Event.on_site
+    @current_event = Event.find_by_id(flash[:current_event_id])
   end
 
   def bydate
+    flash[:current_event_id] = Event.on_site.where(["date_trunc('day', date) = ?", params[:date].to_date]).first.id
     redirect_to events_url(:anchor => params[:date])
   end
 end
