8bfbf16b130f8b0a81409e7764e6e1702ca8c628
Vladislav Izoria <vizoria@macbook.local>
2012-03-13 18:13:27 +0400

    AddThis fix

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,8 @@ class EventsController < ApplicationController
   end
 
   def bydate
-    flash[:current_event_id] = Event.on_site.where(["date_trunc('day', date) = ?", params[:date].to_date]).first.id
-    redirect_to events_url(:anchor => params[:date])
+    @current_event = Event.on_site.where(["date_trunc('day', date) = ?", params[:date].to_date]).first
+    
+    flash[:current_event_id] = @current_event.id
   end
 end
